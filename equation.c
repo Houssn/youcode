f98a921e78f12884765a3f067c3bc81365a33350
Houssn <110052280+Houssn@users.noreply.github.com>
2022-09-09 23:29:44 +0200

    Update equation.c

--- equation.c
+++ equation.c
@@ -16,7 +16,7 @@ k=-b+sqrt(d)/2*a;
 h=-b-sqrt(d)/2*a;
 
 if(d<0){printf(" pas de solution");}
-
+else if(d=0){printf("l equation ilya une solution:%f",s)}
 else {printf("l equation ilya deux solution: SOL1 %f ET SOL %f",k,h);}
 return 0;
 }
